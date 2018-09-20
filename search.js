01cca451786ce4f541673f9749c4013783265a1b
Jon Glass <jonglass82@gmail.com>
2018-09-20 09:43:51 -0500

    added new faq topics to HTML

--- search.js
+++ search.js
@@ -3,8 +3,11 @@ $("#searchBar").on("keyup", function(){
    var userInput = $(this).val().toLowerCase();
     
     
-    $(".q-and-a").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
+    $(".q-and-a, .click-reveal source collapsed, faq-answer click-reveal target collapsed, #faq-intro, h3 ").filter(function() {
+        
+    $(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
+        
+      
   });
     
     
