bc8090fb35649933e2fcdba61d71941562636942
Jon Glass <jonglass82@gmail.com>
2018-09-21 16:14:20 -0500

    added search highlight test function

--- search.js
+++ search.js
@@ -1,15 +1,36 @@
+//highlight function
+
+function searchHighlight(searchWords){
+    
+    if(searchWords){
+    
+    var content = $("p").text();
+    var regExp = new RegExp(searchWords, "ig");
+    var matches = content.match(regExp);
+    
+    if (matches){
+        $("p").html(content.replace(regExp, function(match){
+            return "<span class='highlight'>" + match + "</span>";
+        }));
+    }
+}
+    
+};
+
+
+
 $("#searchBar").on("keyup", function(){
     
    var userInput = $(this).val().toLowerCase();
     
+    searchHighlight(userInput);
     
     $(".q-and-a, .click-reveal source collapsed, faq-answer click-reveal target collapsed, #faq-intro, h3 ").filter(function() {
+
+$(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
         
-    $(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
         
-      
   });
-    
     
 });
 
