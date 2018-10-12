990bd18c14b37bd1bb2bb4c3362227be8b461eb3
Jon Glass <jonglass82@gmail.com>
2018-10-12 17:04:27 -0500

    fixed highlight feature

--- search.js
+++ search.js
@@ -1,61 +1,38 @@
 //highlight function
 
 function hiLite(searchWords){
-    
-    // users 
-    //autopay
-    // stripe
-    //batch edit
-    //payroll
-    // wait list
-    //embedded schedule
-    //group charge
-    //features
-    //login
-    //cancel
-    //web store
-    //point of sale
-    //reg fee
-    //discounts
-    //donation
-    //registration
-    //data
-    //reports
-    //email feature
-    
-    if(searchWords){
-        
-        if (searchWords === "user"){
-    
-            $(".user").addClass("highlight");
+
+        if(searchWords){
+            
+        var content = $("h4").text();
+        var regExp = new RegExp(searchWords, "ig");
+        var matches = content.match(regExp);
+
+       if(matches){
+                $("h4").each(function() {
+                var highlight = '<span class="highlight">'+searchWords+'</span>';
+                var text = $(this).text();
+                            
+                $(this).html(text.replace(regExp, highlight)); 
+                    });
+            }
+              else{
+                $(".highlight").removeClass("highlight");
+            }
             
         }
-        
-        if(searchWords === "autopay"){
-            
-            $(".autopay").addClass("highlight");
-            
+      
+        else{
+            $(".highlight").removeClass("highlight");
         }
-    
-        if(searchWords === "stripe"){
-            
-            $(".stripe").addClass("highlight");
-            
-        }
-        
-        
-    }
-    else{
-       $(".highlight").removeClass("highlight");
-       
-  }
-    
 };
 
 
 
 
-$("#searchBar").on("keyup", function(){
+//$("#searchBar").on("keyup", function(){
+    
+$("#searchBar").keyup(function(){
     
    var userInput = $(this).val().toLowerCase();
     
@@ -70,3 +47,57 @@ $(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
     
 });
 
+//function hiLite(searchWords){
+//    
+//    // users 
+//    //autopay
+//    // stripe
+//    //batch edit
+//    //payroll
+//    // wait list
+//    //embedded schedule
+//    //group charge
+//    //features
+//    //login
+//    //cancel
+//    //web store
+//    //point of sale
+//    //reg fee
+//    //discounts
+//    //donation
+//    //registration
+//    //data
+//    //reports
+//    //email feature
+//    
+//    if(searchWords){
+//        
+//        if (searchWords === "user"){
+//    
+//            $(".users").addClass("highlight");
+//            
+//        }
+//        
+//        if(searchWords === "autopay"){
+//            
+//            $(".autopay").addClass("highlight");
+//            
+//        }
+//    
+//        if(searchWords === "stripe"){
+//            
+//            $(".stripe").addClass("highlight");
+//            
+//        }
+//        
+//        
+//    }
+//    else{
+//       $(".highlight").removeClass("highlight");
+//       
+//  }
+//    
+//};
+//
+//
+//
