96f6b5f2d76b8680efde4951886f1c54b515de56
Jon Glass <jonglass82@gmail.com>
2019-11-05 18:06:27 -0600

    refactor

--- search.js
+++ search.js
@@ -1,25 +1,25 @@
-//highlight function
+// The highlight function
 
 function hiLite(searchWords){
 
         if(searchWords){
             
-        var content = $("h4").text();
-        var regExp = new RegExp(searchWords, "ig");
+        var content = $("h4, .faq-answer").text();
+        var regExp = new RegExp(searchWords, "i");
         var matches = content.match(regExp);
 
-       if(matches){
-                $("h4").each(function() {
-                var highlight = '<span class="highlight">'+searchWords+'</span>';
-                var text = $(this).text();
-                            
-                $(this).html(text.replace(regExp, highlight)); 
-                    });
+           if(matches){
+                $("h4, .faq-answer").each(function() {
+
+                    var highlight = '<span class="highlight">'+searchWords+'</span>';
+                    var text = $(this).text();
+                                
+                    $(this).html(text.replace(regExp, highlight)); 
+                });
             }
-              else{
+            else{
                 $(".highlight").removeClass("highlight");
-            }
-            
+            }  
         }
       
         else{
@@ -28,20 +28,16 @@ function hiLite(searchWords){
 };
 
 
+//The filter function
 
-    
 $("#searchBar").keyup(function(){
     
    var userInput = $(this).val().toLowerCase();
     
-    hiLite(userInput);
-    
-    $(".q-and-a, .click-reveal source collapsed, faq-answer click-reveal target collapsed, #faq-intro, h3 ").filter(function() {
+    $(".q-and-a, .click-reveal source collapsed, .faq-answer click-reveal target collapsed, #faq-intro, h3 ").filter(function() {
 
-$(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
-        
-        
-});
-    
+        $(this).toggle($(this).text().toLowerCase().indexOf(userInput) > -1);
+        hiLite(userInput);
+    });
 });
 
